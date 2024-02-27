8acdfe9764c0b3613d2182944d0ef1ef300c7dd1
LeoBatata <149201381+LeoBatata@users.noreply.github.com>
2024-02-27 18:40:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,95 @@
+//Criar uma referência para tela
+canvas= document.getElementById('myCanvas');
+ctx= canvas.getContext("2d")
+//Dê altura e largura específicas para a imagem do carro
+greencar_width = 75;
+greencar_height = 100;
+backgroundImage = "parkingLot.jpg";
+greencarImage = "car2.png";
+
+//Defina a posição inicial para uma imagem de carro.
+greencar_x= 5;
+greencar_y=225;
+function add() {
+	//carregar carro e imagens de fundo na tela.
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground
+	background_imgTag.scr = background_image
+
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadGreenCar;
+	background_imgTag.scr = greencarImage
+}
+
+function uploadBackground() {
+	//Defina a função ‘uploadBackground’
+ctx.drawImage(background_imgTag, 0, 0 canvas,width, canvas.height)
+}
+
+function uploadGreenCar() {
+	//Defina a função ‘uploadGreenCar’.
+
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height)
+}
+
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	//Definir função para mover o carro para cima
+greencar_y = greencar_y - 10
+uploadBackground();
+}
+
+function down()
+{
+	//Definir função para mover o carro para baixo
+	greencar_y = greencar_y + 10
+	uploadBackground();
+}
+
+function left()
+{
+	//Definir função para mover o carro para o lado esquerdo
+	greencar_x = greencar_x - 10
+	uploadBackground();
+}
+ 
+function right()
+{
+	//Definir função para mover o lado direito do carro
+	greencar_x = greencar_x + 10
+	uploadBackground();
+}
